56498c18ffc0fa387401c190b311deb9df81816b
AnnaIstochnikova <anya.istochnikova@gmail.com>
2023-11-02 19:49:31 +0100

    Refactor code using useState and useEffect hooks

--- src/components/phonebook/phonebook.js
+++ src/components/phonebook/phonebook.js
@@ -1,59 +1,38 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import { Filter } from './filter/filter';
 import { ContactList } from './contact-list/contactList';
 import { ContactForm } from './contact-form/contactForm';
 
-export class Phonebook extends Component {
-  state = {
-    contacts: [],
-    showContactList: false,
-    filter: '',
-  };
+export const Phonebook = () => {
+  const [contacts, setContacts] = useState([]);
+  const [showContactList, setShowContactList] = useState(false);
+  const [filter, setFilter] = useState('');
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter)
+  );
 
-  deleteContact = contact => {
-    this.setState(prevState => {
+  const deleteContact = contact => {
+    setContacts(prev => {
+      console.log(prev);
       const contactsAfterDelete = [
-        ...prevState.contacts.filter(oldContact => oldContact !== contact),
+        ...prev.filter(oldContact => oldContact !== contact),
       ];
       localStorage.setItem('Contacts', JSON.stringify(contactsAfterDelete));
       if (contactsAfterDelete.length === 0) {
-        return {
-          contacts: contactsAfterDelete,
-          showContactList: false,
-        };
+        setShowContactList(false);
       }
-      return {
-        contacts: contactsAfterDelete,
-      };
+      return contactsAfterDelete;
     });
   };
 
-  findContact = event => {
+  const findContact = event => {
     event.preventDefault();
-    this.setState({ filter: event.target.value.toLowerCase() });
+    setFilter(event.target.value.toLowerCase());
   };
 
-  componentDidMount() {
-    try {
-      const contactsFromLocalStorage = localStorage.getItem('Contacts');
-      if (contactsFromLocalStorage.length === 2) {
-        this.setState({ showContactList: false });
-      } else {
-        this.setState(() => {
-          return {
-            contacts: JSON.parse(contactsFromLocalStorage),
-            showContactList: true,
-          };
-        });
-      }
-    } catch (error) {
-      console.error('Get state error: ', error.message);
-    }
-  }
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
     const form = event.currentTarget;
     const name = form.elements.name.value;
@@ -64,57 +43,61 @@ export class Phonebook extends Component {
       phoneNumber: phoneNumber,
     };
 
-    this.setState(prevState => {
-      for (const element of prevState.contacts) {
-        if (element.name === name) {
-          alert(`${name} is already in contacts`);
-          return;
-        }
+    for (const element of contacts) {
+      if (element.name === name) {
+        alert(`${name} is already in contacts`);
+        return;
       }
-
-      const allContacts = [...prevState.contacts, newContact];
+    }
+    setContacts(prev => {
+      const allContacts = [...prev, newContact];
       localStorage.setItem('Contacts', JSON.stringify(allContacts));
-      return {
-        contacts: allContacts,
-        showContactList: true,
-      };
+      setShowContactList(true);
+      return allContacts;
     });
     form.reset();
   };
-  render() {
-    const { contacts, filter, showContactList } = this.state;
-    const filteredContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter)
-    );
-    return (
-      <>
-        <h1>Phonebook</h1>
-        <ContactForm handleSubmitFn={this.handleSubmit} />
-        {showContactList && (
-          <>
-            <Filter filterFn={this.findContact} />
-          </>
-        )}
-        {showContactList && (
-          <>
-            <h2>Contacts</h2>
-            <ContactList
-              allContacts={filteredContacts}
-              onDelete={this.deleteContact}
-            />
-          </>
-        )}
-      </>
-    );
-  }
-}
+
+  useEffect(() => {
+    try {
+      const contactsFromLocalStorage = localStorage.getItem('Contacts');
+      if (contactsFromLocalStorage.length === 2) {
+        setShowContactList(false);
+      } else {
+        setContacts(JSON.parse(contactsFromLocalStorage));
+        setShowContactList(true);
+      }
+    } catch (error) {
+      console.error('Get state error: ', error.message);
+    }
+  }, []);
+
+  return (
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm handleSubmitFn={handleSubmit} />
+      {showContactList && (
+        <>
+          <Filter filterFn={findContact} />
+        </>
+      )}
+      {showContactList && (
+        <>
+          <h2>Contacts</h2>
+          <ContactList
+            allContacts={filteredContacts}
+            onDelete={deleteContact}
+          />
+        </>
+      )}
+    </>
+  );
+};
 
 Phonebook.propTypes = {
-  state: PropTypes.shape({
-    contacts: PropTypes.array,
-    showContactList: PropTypes.bool,
-    filter: PropTypes.string,
-  }),
+  contacts: PropTypes.array,
+  showContactList: PropTypes.bool,
+  filter: PropTypes.string,
   deleteContact: PropTypes.func,
   findContact: PropTypes.func,
   handleSubmit: PropTypes.func,
